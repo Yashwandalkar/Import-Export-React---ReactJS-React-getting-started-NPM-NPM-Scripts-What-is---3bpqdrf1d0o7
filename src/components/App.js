e9a0c16625f67dcf39870ee92543d802a400339e
Yash Wandalkar <yash.wandalkar@gmail.com>
2022-09-19 07:40:31 +0000

    Update 6 files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Heading from "heading";
+import InputQuery from "inputquery";
+import SubHeading from "subheading";
+import SubmitButton from "submitbutton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading />
+      <InputQuery />
+      <SubmitButton />
     </div>
-  )
-}
-
-
+  );
+};
+export default App;
